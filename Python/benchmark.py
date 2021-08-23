7dc31fb2a8790cb65e86d67030c233e1bbea8741
Alexander Hunt <alexander.hunt@ed.ac.uk>
2021-08-23 22:34:13 +0200

    added benchmark

---
+++ Python/benchmark.py
@@ -0,0 +1,5 @@
+from ai_benchmark import AIBenchmark
+
+#Reu benchmark for tensorflow 
+benchmark = AIBenchmark(use_CPU=None, verbose_level=3)
+results = benchmark.run(precision="high")
--- laucher.sh
+++ laucher.sh
@@ -2,17 +2,21 @@
 echo "Welcome to the program launcher"
 echo "Be sure to have the requirements installed"
 PS3='Choose what you would like to do today: '
-option=("TFTEST" "More" "Quit")
+option=("TFTEST" "Benchmark" "More" "Quit")
 select fav in "${option[@]}"; do
     case $fav in
         "TFTEST")
             echo "Testing out for compatible GPUs (AMD ROC GPUs won't appear unless Tensorflow compiled to use ROC"
             python Python/TF_test_GPU.py
             ;;
+        "Benchmark")
+            echo "Benchmarking Tensorflow Setup"
+            python Python/benchmark.py
+            ;;
         "More")
             echo "Come back later for more options"
             echo "thank you, see you again soon"
-	    exit
+        exit
             ;;
         "Quit")
             echo "User requested exit"
