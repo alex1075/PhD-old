3f19b262928339bf0b5bfff119317fcbddfce2fe
Alexander Hunt <alexander.hunt@ed.ac.uk>
2021-10-21 16:55:54 +0100

    Branching until stable

--- code/tester.py
+++ code/tester.py
@@ -1,6 +1,6 @@
 
 import tensorflow as tf
-
+from ai_benchmark import AIBenchmark
 
 def tf_test():
  tf.test.gpu_device_name(),
@@ -8,4 +8,8 @@ def tf_test():
  print(tf.test.is_built_with_cuda()),
  return
 
-
+def benchmark():
+    #Reu benchmark for tensorflow 
+    benchmark = AIBenchmark(use_CPU=None, verbose_level=3)
+    # results = benchmark.run(precision="high")
+    result = benchmark.run()
--- neoLaucher.sh
+++ neoLaucher.sh
@@ -2,57 +2,46 @@
 echo "Welcome to the program launcher"
 echo "Be sure to have the requirements installed"
 PS3='Choose what you would like to do today: '
-option=("Pre-Processing" "Training" "Other" "Quit")
+option=("Pre-Processing" "Training" "Quit")
 select fav in "${option[@]}"; do
     case $fav in
         "Pre-Processing")
-            echo "Welcome to the program launcher"
-            echo "Be sure to have the requirements installed"
-            PS3='Choose what you would like to do today: '
-            option=("TFTEST" "Benchmark" "Convert dataset to jpeg" "Convert video to jpeg" "Quit")
+            PS3='Choose your what pre-processing you would like to do:'
+            option=("TFTEST" "Benchmark" "Convert dataset to jpeg" "Convert video to jpeg" "Normalise dataset" "Quit")
             select fav in "${option[@]}"; do
                case $fav in
                     "TFTEST")
                         echo "Testing out for compatible GPUs (AMD ROC GPUs won't appear unless Tensorflow compiled to use ROC"
-                        python Python/TF_test_GPU.py
+                        python -c "from code.tester import tf_test ; tf_test()" 
                         ;;
                     "Benchmark")
                        echo "Benchmarking Tensorflow Setup"
-                        python Python/benchmark.py
+                        python -c "from code.tester import benchmark ; benchmark()"
                         ;;
                     "Convert dataset to jpeg")
                         echo "Ensure the dataset path is correct"
                         echo "Beginning conversion"
-                        python -c 'from convert import convert; convert()'  
+                        python -c 'from code.convert import convert; convert()'  
                        ;;
                     "Convert video to jpeg")
                         echo "Ensure the video is in the correct place"
                         echo "Beginning conversion"
-                        python Python/make_image.py
+                        python -c 'from code.convert import convertVideoToImage; convertVideoToImage()'
+                    ;;
+                    "Normalise dataset")
+                        echo "Ensure the dataset has been resized and imported properly"
+                        echo "Beginning normalisation"
+                        python -c "from code.convert import * ; normalise()"
                     ;;
                     "Quit")
                         echo "User requested exit"
-                        exit
+                        exit 
                         ;;
-                    *) echo "invalid option $REPLY";;
                 esac
-            done
-        "Benchmark")
-            echo "Benchmarking Tensorflow Setup"
-            python Python/benchmark.py
+        "Training")
+            echo "Section under construction"
+            
             ;;
-        "Convert dataset to jpeg")
-            echo "Ensure the dataset path is correct"
-            echo "Beginning conversion"
-            python Python/convert.py
-            exit
-            ;;
-        "Convert video to jpeg")
-            echo "Ensure the video is in the correct place"
-            echo "Beginning conversion"
-            python Python/make_image.py
-            exit
-           ;;
         "Quit")
             echo "User requested exit"
             exit
